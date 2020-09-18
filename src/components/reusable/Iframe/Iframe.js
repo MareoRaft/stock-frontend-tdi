85f69cbadb96ff48033c6a68a181ec3a311ef79c
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-09-18 11:21:38 -0400

    grab URL from an env variable

--- src/components/reusable/Iframe/Iframe.js
+++ src/components/reusable/Iframe/Iframe.js
@@ -2,19 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Iframe from 'react-iframe'
 
+let URL = process.env['REACT_APP_BACKEND_URL']
+console.log('URL is ' + URL)
+
+
 
 class Iframe2 extends React.Component {
 	render() {
 		return (
-			<Iframe
-				url="http://localhost:5000/"
-		        width="450px"
-		        height="450px"
-		        id="myId"
-		        className="myClassname"
-		        display="initial"
-		        position="relative"
-			/>
+			<Iframe {...{
+				url: URL,
+		        width: "450px",
+		        height: "450px",
+		        id: "myId",
+		        className: "myClassname",
+		        display: "initial",
+		        position: "relative",
+			}}/>
         );
 	}
 }
