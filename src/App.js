6cb1f2079c37ac5e03abf76cc4bb9106305e0309
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-09-18 14:30:53 -0400

    make frontend handle multiple stats

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React, {useState} from 'react'
+import _ from 'lodash'
+
 import logo from './logo.svg'
 import './App.css'
 import Iframe from './components/reusable/Iframe'
+const STAT_TYPES = ['open', 'high', 'low', 'close']
+
+const renderCheckbox = (stat, statsToDisplay, handleCheckboxClick) => {
+  return <div>
+    <input
+      type="checkbox"
+      id={stat}
+      name={stat}
+      checked={statsToDisplay.has(stat)}
+      onChange={() => handleCheckboxClick(stat)}
+    />
+    <label for={stat}>
+      {stat} price
+    </label>
+  </div>
+}
+
+const renderCheckboxes = (statsToDisplay, handleCheckboxClick) => {
+  return STAT_TYPES.map((stat) => renderCheckbox(stat, statsToDisplay, handleCheckboxClick))
+}
+
 
 function App() {
   const [tickerInput, setTickerInput] = useState('VZ')
   const [url, setUrl] = useState(process.env['REACT_APP_BACKEND_URL'])
+  const [statsToDisplay, setStatsToDisplay] = useState(new Set())
   const handleChangeTickerInput = (event) => {
     setTickerInput(event.target.value)
   }
   const handleButtonClick = (event) => {
     const base_url = process.env['REACT_APP_BACKEND_URL']
-    const new_url = `${base_url}?ticker=${tickerInput}`
+    const stat_param_strings = STAT_TYPES.map((stat) => `${stat}=${statsToDisplay.has(stat)}`)
+    const stat_param_string = stat_param_strings.join('&')
+    const new_url = `${base_url}?ticker=${tickerInput}&${stat_param_string}`
     setUrl(new_url)
+  }
+  const handleCheckboxClick = (stat) => {
+    let stats_to_display = _.clone(statsToDisplay)
+    // toggle the stat that they clicked
+    if (stats_to_display.has(stat)) {
+      stats_to_display.delete(stat)
+    }
+    else {
+      stats_to_display.add(stat)
+    }
+    setStatsToDisplay(stats_to_display)
   }
   return (
     <div className="App">
@@ -23,16 +60,16 @@ function App() {
           value={tickerInput}
           onChange={handleChangeTickerInput}
         />
+        <br/>
+        {renderCheckboxes(statsToDisplay, handleCheckboxClick)}
+        <br/>
         <button
           type='button'
           onClick={handleButtonClick}
         >
           fetch stock data
         </button>
-
-
-
-
+        <br/>
         <Iframe {...{
           url: url,
         }}/>
