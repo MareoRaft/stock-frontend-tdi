511194e6a7574ec015e3486e87dfa52524891e59
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-09-18 11:48:54 -0400

    add ticker symbol controlled GUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
-import Char from './components/one-time/Char'
 import Iframe from './components/reusable/Iframe'
 
 function App() {
+  const [tickerInput, setTickerInput] = useState('VZ')
+  const handleChangeTickerInput = (event) => {
+    setTickerInput(event.target.value)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.  TDI.
-        </a>
+        Ticker symbol:
+        <input
+          type='text'
+          value={tickerInput}
+          onChange={handleChangeTickerInput}
+        />
+        tickervalue preview:
+        {tickerInput}
+
+
+
+
         <Iframe>i am the iframe component</Iframe>
         done char.
       </header>
